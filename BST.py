b645906d99d7861a2b5d3774b20542d8c307191d
Prashant Kolhar <60572402+Kolhar-Prashant@users.noreply.github.com>
2021-05-04 19:09:38 +0530

    Add files via upload

---
+++ BST.py
@@ -0,0 +1,54 @@
+
+def check_if_deadend_node(indx):
+    temp = List[:indx+1]
+    if List[indx] == 1:
+        if List[indx]+1 in temp:
+            return List[indx]
+    if List[indx]-1 in temp and List[indx]+1 in temp:
+        return List[indx]
+    else:
+        return -1
+
+
+class BST:
+    def __init__(self,num):
+        self.val = num
+        self.left = None
+        self.right = None
+
+    def insert(self,num,root):
+        temp = root
+        if num < temp.val:
+            if temp.left == None:
+                t = BST(num)
+                temp.left = t
+            else:
+                root = temp.left
+                BST.insert(self,num,root)
+        else:
+            if temp.right == None:
+                t = BST(num)
+                temp.right = t
+            else:
+                root = temp.right
+                BST.insert(self,num,root)
+
+List = [8,5,9,2,7,11]
+indx = 0
+Tree = ""
+root = ""
+Dead_end = False
+while indx != len(List):
+    if indx == 0:
+        Tree = BST(List[indx])
+        root = Tree
+    else:
+        if check_if_deadend_node(indx) == -1:
+            Tree.insert(List[indx],root)
+        else:
+            print("Node",str(List[indx]),"will be the Dead-end node")
+            Dead_end = True
+    indx+=1
+if Dead_end == False:
+    print("This list won't have any Dead-end node")
+
